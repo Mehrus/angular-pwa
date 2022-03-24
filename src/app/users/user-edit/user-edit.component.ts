baa064884d5da1fb13fa796ffb9d241871d29fcb
p.goral <17201199+Mehrus@users.noreply.github.com>
2022-03-24 18:04:51 +0100

    Show Confirmation ( Are you sure ? Yes | No ) Dialog

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MaterialDialog } from './dialog/material-dialog';
 
 @Component({
   selector: 'app-user-edit',
@@ -10,7 +12,7 @@ export class UserEditComponent implements OnInit {
   firstName: string = '';
   lastName: string = '';
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.firstName = 'Hugh';
@@ -19,6 +21,18 @@ export class UserEditComponent implements OnInit {
 
   getFullname() {
     return `${this.firstName} ${this.lastName}`;
+  }
+
+  openDialog(): void {
+    console.log('open dialog');
+    const dialogRef = this.dialog.open(MaterialDialog, {
+      width: '250px',
+      panelClass: 'custom-modal'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
--- src/app/users/user-edit/user-edit.module.ts
+++ src/app/users/user-edit/user-edit.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { UserEditRoutingModule } from './user-edit-routing.module';
 import { UserEditComponent } from './user-edit.component';
 import { PrimaryButtonComponent } from 'src/app/parts/primary-button/primary-button.component';
+import { MaterialDialog } from './dialog/material-dialog';
 
 
 @NgModule({
   declarations: [
     UserEditComponent,
+    MaterialDialog,
     PrimaryButtonComponent
   ],
   imports: [
