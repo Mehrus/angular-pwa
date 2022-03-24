b20b9dced31f8dedf9e0afe3b2e4266f434e8b44
p.goral <17201199+Mehrus@users.noreply.github.com>
2022-03-24 13:22:18 +0100

    Prepare all routes with dynamic loading - use Angular CLI with --routing flag

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users-list', loadChildren: () => import('./users/users-list/users-list.module').then(m => m.UsersListModule)},
+  { path: 'user-edit', loadChildren: () => import('./users/user-edit/user-edit.module').then(m => m.UserEditModule) },
+  { path: 'settings', loadChildren: () => import('./users/settings/settings.module').then(m => m.SettingsModule) },
+  { path: 'roles-and-permissions', loadChildren: () => import('./users/roles-and-permissions/roles-and-permissions.module').then(m => m.RolesAndPermissionsModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { RolesAndPermissionsComponent } from './users/roles-and-permissions/roles-and-permissions/roles-and-permissions.component';
+import { SettingsComponent } from './users/settings/settings/settings.component';
+import { UserEditComponent } from './users/user-edit/user-edit/user-edit.component';
+import { UsersListComponent } from './users/users-list/users-list/users-list.component';
+import { NavigationComponent } from './header/navigation/navigation.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RolesAndPermissionsComponent,
+    SettingsComponent,
+    UserEditComponent,
+    UsersListComponent,
+    NavigationComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
