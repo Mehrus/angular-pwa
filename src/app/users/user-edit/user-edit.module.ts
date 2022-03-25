5ecc4eb5f7333469bc59a60ab65f191f2462d04b
p.goral <17201199+Mehrus@users.noreply.github.com>
2022-03-25 11:13:24 +0100

    Prepare Active / InActive Tab state

--- src/app/users/user-edit/user-edit.module.ts
+++ src/app/users/user-edit/user-edit.module.ts
@@ -4,16 +4,19 @@ import { CommonModule } from '@angular/common';
 import { UserEditRoutingModule } from './user-edit-routing.module';
 import { UserEditComponent } from './user-edit.component';
 import { PrimaryButtonComponent } from 'src/app/parts/primary-button/primary-button.component';
-
+import { UserRoleComponent } from './user-role/user-role.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
     UserEditComponent,
-    PrimaryButtonComponent
+    PrimaryButtonComponent,
+    UserRoleComponent,
   ],
   imports: [
     CommonModule,
-    UserEditRoutingModule
+    UserEditRoutingModule,
+    MatTabsModule
   ]
 })
 export class UserEditModule { }
